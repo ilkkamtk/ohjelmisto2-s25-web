9dff062200f1be8d4e591fa7483f61fa9cff995a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-11-18 14:01:57 +0200

    update

--- moduuli4/esimerkki/esimerkki.js
+++ moduuli4/esimerkki/esimerkki.js
@@ -1,21 +1,16 @@
 'use strict';
-console.log('the script starts');
 
-function synchronousFunction() {
-  let number = 1;
-  for (let i = 1; i < 100000; i++) {
-    number += i;
-    console.log('synchronousFunction running');
-  }
-  console.log('regular function complete', number);
-}
+const airportForm = document.querySelector('#airport-form');
 
-async function asynchronousFunction() {                 // asynchronous function is defined by the async keyword
-  console.log('asynchronous download begins');
+async function asynchronousFunction(evt) {
+  evt.preventDefault();
+  // get value of input element
+  const code = document.querySelector('input[name=icao]').value;
   try {                                               // error handling: try/catch/finally
-    const response = await fetch('http://127.0.0.1:3000/airport/00A');    // starting data download, fetch returns a promise which contains an object of type 'response'
+    const response = await fetch('http://127.0.0.1:3000/airport/' + code);    // starting data download, fetch returns a promise which contains an object of type 'response'
     const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function
-    console.log(jsonData.ICAO, jsonData.Name);    // log the result to the console
+    document.querySelector(
+        '#target').innerText = `Nimi: ${jsonData.name}, Kunta: ${jsonData.municipality} `;
   } catch (error) {
     console.log(error.message);
   } finally {                                         // finally = this is executed anyway, whether the execution was successful or not
@@ -23,7 +18,4 @@ async function asynchronousFunction() {                 // asynchronous function
   }
 }
 
-synchronousFunction();
-asynchronousFunction();
-
-console.log('the script ends');
+airportForm.addEventListener('submit', asynchronousFunction);
